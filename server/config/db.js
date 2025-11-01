011d14d961f562c74ea6ab43dfe57806a482a7ff
angel1701-lap <1538852@senati.pe>
2025-10-31 19:21:13 -0500

    cambios de la configuracion de la base de datos

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'zapatillas',
+  database: process.env.DB_NAME || 'zapatillas-db',
   waitForConnections: true,
   connectionLimit: 10,
   namedPlaceholders: true,
