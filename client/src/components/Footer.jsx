e494d23f66f8195fafe1c9238cbbdc94727a4274
Esteban Martos <esteban439299@gmail.com>
2025-11-09 17:41:48 -0500

    Actualizacion del footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-5">
+      <p className="mb-0">© 2025 ZapaStore | Todos los derechos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
