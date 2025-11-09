72e81d581c1ddd9b490cfd0d1773eb7ae7d1be9a
Esteban Martos <esteban439299@gmail.com>
2025-11-09 18:26:44 -0500

    cambio

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <div className="bg-light py-5 text-center">
-        <h1 className="fw-bold">Bienvenido a ZapaStore 👟</h1>
+        <h1 className="fw-bold">Bienvenido a ZapaStore </h1>
         <p className="text-muted">Encuentra tus zapatillas favoritas al mejor precio</p>
       </div>
       <ProductList />
