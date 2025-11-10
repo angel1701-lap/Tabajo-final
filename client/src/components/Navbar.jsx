7b51cf9fd67e7e3bcd117e6b4eea34d0e5243cf7
Esteban Martos <esteban439299@gmail.com>
2025-11-10 02:27:01 -0500

    Actualizacion en navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
-function Navbar() {
+function Navbar({ cartCount, onNavigate }) {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand mx-auto fs-3 fw-bold" href="#">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <a
+          className="navbar-brand fs-3 fw-bold"
+          href="#"
+          onClick={() => onNavigate('home')}
+        >
            ZapaStore
         </a>
+
+        <div className="d-flex gap-2">
+          <button
+            className="btn btn-outline-light"
+            onClick={() => onNavigate('prediccion')}
+          >
+             Predicción
+          </button>
+
+          <button
+            className="btn btn-outline-light position-relative"
+            onClick={() => onNavigate('cart')}
+          >
+             Carrito
+            {cartCount > 0 && (
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {cartCount}
+              </span>
+            )}
+          </button>
+        </div>
       </div>
     </nav>
   );
